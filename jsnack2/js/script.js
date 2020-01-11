3f6ebfc78113db952c6588e2958f297bf5726eaf
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-11 15:16:00 +0100

    aggiunto check riempimento campi snack 2

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -3,6 +3,15 @@
 var parolaUno= prompt ('inserisci una parola');
 var parolaDue= prompt ('inserisci un\'altra parola');
 
+while (parolaUno.length <=0 || parolaDue.length <=0){
+  if(parolaUno.length <=0){
+   parolaUno = prompt('il campo della parolaUno non può essere vuoto, inserisci una parola');
+  }
+  else if (parolaDue.length <=0) {
+    parolaDue = prompt('il campo della parolaDue non può essere vuoto, inserisci una parola');
+  }
+}
+
 if (parolaUno.length < parolaDue.length){
   console.log(parolaUno + ' ' + parolaDue);
 }
