cb03e8245c1dae9cf02e77894105d555cc679c22
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-11 14:56:11 +0100

    restyling codice snack 1

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -7,7 +7,7 @@ if (primoNumero > secondoNumero){
   console.log(primoNumero);
 }
 else if (primoNumero == secondoNumero){
-  alert ('Hai inserito due numeri uguali!')
+  alert ('Hai inserito due numeri uguali!');
 }
 else {
   console.log(secondoNumero);
